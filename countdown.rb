2eb13615a236fe2145fca41caf6c077ed0c25f33
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-05 14:48:36 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
 
-def countdown
+def countdown(integer)
+  integer = 10 
+  while integer > 0 
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(second)
+  second = 10
+  while second > 0
+    second -= 1
+    sleep 1
+  end
 end
